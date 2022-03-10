30706d998cc1980d5ffe1c286b07a2819be55227
Marian Ilie <ilie.marian32@yahoo.com>
2022-03-10 20:35:59 +0000

    delete request

--- blog/src/BlogDetails/blogDetails.js
+++ blog/src/BlogDetails/blogDetails.js
@@ -1,14 +1,24 @@
 import { useParams } from "react-router-dom";
 import useFetch from "../UseFetch/useFetch";
+import { useHistory } from "react-router-dom";
 
 const BlogDetails = () => {
   const { id } = useParams();
+  const history = useHistory();
   const {
     data: blog,
     error,
     isPending,
   } = useFetch("http://localhost:8000/blogs/" + id);
 
+  const handleClick = () => {
+    fetch("http://localhost:8000/blogs/" + blog.id, {
+      method: "DELETE"
+    
+    }).then(() => {
+      history.push("/");
+    });
+  };
   return (
     <div className="blog-details">
       {isPending && <div> Loading...</div>}
@@ -18,6 +28,7 @@ const BlogDetails = () => {
           <h2>{blog.title}</h2>
           <p> Written by {blog.author}</p>
           <div>{blog.body}</div>
+          <button onClick={handleClick}>Delete</button>
         </article>
       )}
     </div>
